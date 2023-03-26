9489e3171035e409fdd22ba516c36b6939318561
Georgefdz <georgfdz@gmail.com>
2023-03-26 17:48:07 -0600

    changed cloudinary.js to fix an api error

--- middleware/cloudinary.js
+++ middleware/cloudinary.js
@@ -8,4 +8,7 @@ cloudinary.config({
   api_secret: process.env.API_SECRET,
 });
 
+cloudinary.uploader.upload("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==", 
+  function(error, result) {console.log(result, error); });
+  
 module.exports = cloudinary;
